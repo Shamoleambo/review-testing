c5705a26ca67da5635b70e76b9d722adef22c3e6
Tiago Gomes <tidgomes@gmail.com>
2024-10-14 15:11:21 -0300

    first commit

---
+++ src/main/java/com/tidz/review_testing/ReviewTestingApplication.java
@@ -0,0 +1,13 @@
+package com.tidz.review_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tidz/review_testing/ReviewTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tidz.review_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
