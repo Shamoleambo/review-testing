229bd93c9f1b8fba50d646a47bc2d4a8a0327dd5
Tiago Gomes <tidgomes@gmail.com>
2024-10-14 15:25:21 -0300

    create Car model

---
+++ src/main/java/com/tidz/review_testing/model/Car.java
@@ -0,0 +1,75 @@
+package com.tidz.review_testing.model;
+
+import jakarta.persistence.*;
+
+import java.time.Year;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private CarType type;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "year", nullable = false)
+    private Year year;
+
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    private Owner owner;
+
+    public Car() {
+
+    }
+
+    public Car(CarType type, String name, Year year) {
+        this.type = type;
+        this.name = name;
+        this.year = year;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public CarType getType() {
+        return type;
+    }
+
+    public void setType(CarType type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Year getYear() {
+        return year;
+    }
+
+    public void setYear(Year year) {
+        this.year = year;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+}
