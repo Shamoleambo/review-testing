c52b2091e343907ef538bb0978ebabfc806d0291
Tiago Gomes <tidgomes@gmail.com>
2024-10-14 15:30:20 -0300

    create Owner class

---
+++ src/main/java/com/tidz/review_testing/model/Owner.java
@@ -0,0 +1,88 @@
+package com.tidz.review_testing.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "owners")
+public class Owner {
+
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDate birthDate;
+
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
+    private List<Car> cars = new ArrayList<>();
+
+    public Owner() {
+
+    }
+
+    public Owner(String firstName, String lastName, String email, LocalDate birthDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.birthDate = birthDate;
+    }
+
+    public void addNewCar(Car car) {
+        cars.add(car);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
+}
