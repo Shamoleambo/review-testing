41f540ea36bb798dc5d7120676a192acd3e837d7
Tiago Gomes <tidgomes@gmail.com>
2024-10-18 14:13:30 -0300

    create default api response

---
+++ src/main/java/com/tidz/review_testing/http/ApiResponse.java
@@ -0,0 +1,28 @@
+package com.tidz.review_testing.http;
+
+public class ApiResponse {
+
+    private String message;
+    private Object body;
+
+    public ApiResponse(String message, Object body) {
+        this.message = message;
+        this.body = body;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getBody() {
+        return body;
+    }
+
+    public void setBody(Object body) {
+        this.body = body;
+    }
+}
